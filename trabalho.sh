1d50d55ba7104cd5c547cc517d063c686ec21377
olivercoutinho <olicop@gmail.com>
2024-11-22 15:39:58 -0300

    teste

--- trabalho.sh
+++ trabalho.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
 # NAO APAGUE A LINHA ACIMA!
-teste
+cd ~
 
+mkdir joao maria
+
+cp -r /etc/systemd joao/
+
+cp /etc/passwd /etc/group maria/
+
+cp -r maria maria_copia
+
+cp -r joao joao_copia
+
+mkdir mais_uma_pasta
+
+cp -r joao maria mais_uma_pasta/
+
+mv mais_uma_pasta/joao mais_uma_pasta/x
+
+mv mais_uma_pasta/maria mais_uma_pasta/joao
+
+mv mais_uma_pasta/x mais_uma_pasta/maria
